67f6bec28b67302e1021a2a449dba79bff5b0def
Christian Glahn <cpglahn@gmail.com>
2017-02-16 17:50:09 +0100

    generic code

--- zipfinder.js
+++ zipfinder.js
@@ -1,44 +1,48 @@
-var path = require('path'), fs=require('fs');
 
+var path = require('path'),
+    fs   = require('fs');
+
+// This finds all relevant zip files.
 function selectFiles(startPath,filter) {
 
     //console.log('Starting from dir '+startPath+'/');
-    var zipFiles = [];
+    var theFiles = [];
 
     if (fs.existsSync(startPath)) {
         var files = fs.readdirSync(startPath);
-        
-        // the following is a bit 
-        zipfilesFound = files
-            .map(function (filename) {
-                var zip  = filename.split(".").pop();
-                filename = path.join(startPath, filename);
+
+        // Map-Reduce through the directories
+        // 1. map all files and recurse into the subdirectories
+        // 2. reduce the files into a flat array.
+        //
+        // theFiles will contain the result for the reduce() call.
+        theFiles = files
+            .map(function fetchFiles(filename) {                           // 1
+                var result = undefined;
+                var suffix = filename.split(".").pop();
+                filename   = path.join(startPath, filename);
 
                 if (fs.lstatSync(filename).isDirectory()) {
-                    // this will add an Array to our result set
-                    return selectFiles(filename, filter);
+                    // recursing into a subdirectory will add an Array
+                    result = selectFiles(filename, filter);
                 }
-                else if (zip == filter) {
-                    // place just the filename of the zip into the result set
-                    return filename;
-                } 
+                else if (suffix == filter) {
+                    result = filename;
+                }
+                // always return
+                return result;
             })
-            .reduce(function (resultArray, nameOrArray)  {
+            .reduce(function flattenArray(resultArray, nameOrArray)  {     // 2
                 if (Array.isArray(nameOrArray)) {
-                    resultArray.concat(nameOrArray);
+                    // immediate return to avoid confusing assignments
+                    return resultArray.concat(nameOrArray);
                 }
                 if (nameOrArray !== undefined) {
-                    resultArray.push(filename);
+                    resultArray.push(nameOrArray);
                 }
-                return resultArray;
-            }, zipFiles);
-
-        console.log('the zipfiles i found: ', zipfilesFound);
+                return resultArray; // MUST return the result array
+            }, []); // pass an empty array as reduce initializer
     }
-    return zipFiles;
+
+    return theFiles; // return the final result
 }
-
-var zips = selectFiles("input/", "zip");
-
-console.log(zips);
-
