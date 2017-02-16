aa3d3bec257f4fea56c6a58bb71dc0fc318abfff
Christian Glahn <cpglahn@gmail.com>
2017-02-16 17:28:10 +0100

    code streamline

--- zipfinder.js
+++ zipfinder.js
@@ -3,76 +3,42 @@ var path = require('path'), fs=require('fs');
 function selectFiles(startPath,filter) {
 
     //console.log('Starting from dir '+startPath+'/');
-    var zipfilesFound = [];
+    var zipFiles = [];
 
-    if (!fs.existsSync(startPath)) {
-        console.log("no dir ", startPath);
-        return;
+    if (fs.existsSync(startPath)) {
+        var files = fs.readdirSync(startPath);
+        
+        // the following is a bit 
+        zipfilesFound = files
+            .map(function (filename) {
+                var zip  = filename.split(".").pop();
+                filename = path.join(startPath, filename);
+
+                if (fs.lstatSync(filename).isDirectory()) {
+                    // this will add an Array to our result set
+                    return selectFiles(filename, filter);
+                }
+                else if (zip == filter) {
+                    // place just the filename of the zip into the result set
+                    return filename;
+                } 
+            })
+            .reduce(function (resultArray, nameOrArray)  {
+                if (Array.isArray(nameOrArray)) {
+                    resultArray.concat(nameOrArray);
+                }
+                if (nameOrArray !== undefined) {
+                    resultArray.push(filename);
+                }
+                return resultArray;
+            }, zipFiles);
+
+        console.log('the zipfiles i found: ', zipfilesFound);
     }
-
-    var files = fs.readdirSync(startPath);
-
-    zipfilesFound = files
-        .map(function (filename) {
-            var zip  = filename.split(".").pop();
-            filename = path.join(startPath, filename);
-            var stat = fs.lstatSync(filename); 
-
-            if (fs.lstatSync(filename).isDirectory()) {
-                selectFiles(filename, zip);
-            }
-            else if (zip == filter) {
-                console.log(filename);
-                return filename;
-            } 
-        })
-        .reduce(function (resultArray, filename)  {
-            if (filename !== undefined) {
-                resultArray.push(filename);
-            }
-            return resultArray;
-        }, []);
-
-    // for (var i = 0; i < files.length; i++) {
-    //     var filename = path.join(startPath, files[i]);
-    //     var stat = fs.lstatSync(filename);
-    
-    //     if (stat.isDirectory()) {
-    //         fromDir(filename, filter); //recurse
-    //     }
-    //     else if (filename.indexOf(filter)>=0) {
-    //         console.log('-- found: ;
-    //         zipfilesFound.push(filename);
-    //     }
-    // }
-
-    console.log('the zipfiles i found: ', zipfilesFound);
-
-    return zipfilesFound;
-}
-
-function recourseDir(startPath) {    
-    console.log("xxx");
-
-    var files = fs.readdirSync(startPath);    
-
-    var zipFiles = files
-        .map(function(filename) {
-            filename = path.join(startPath, filename);
-            if (fs.lstatSync(filename).isDirectory()) {
-                var tzip = selectFiles(filename, "zip");       
-            }
-            return [];
-        })
-        .reduce(function (resultArray, fileList) {
-
-            return resultArray.concat(fileList);
-        }, []);
-
     return zipFiles;
 }
 
-var zips = recourseDir("input/");
+var zips = selectFiles("input/", "zip");
 
 console.log(zips);
-//fromDir('input/','zip');
+
